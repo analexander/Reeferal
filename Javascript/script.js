53650609b75656d224dcb2a2f4c36258e159fe60
Angel Alexander <angale22@gmail.com>
2020-08-12 17:14:24 -0500

    added new css framework picnicss, added dummy navbar

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,3 +1,6 @@
+
+//----------------
+
 var queryURL = "https://strainapi.evanbusse.com/zOfVj0g/searchdata/effects";
 
     $.ajax({
@@ -10,28 +13,5 @@ var queryURL = "https://strainapi.evanbusse.com/zOfVj0g/searchdata/effects";
 
     });
 
-  // Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
+document.getElementById('modal_1').checked = true; // open modal
+document.getElementById('modal_1').checked = false; // close modal
